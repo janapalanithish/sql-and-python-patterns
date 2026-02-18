4869c5d06fbb549e2376cbbb0502ff32392dd31b
nithish kumar <janapalanithish1982715@gmail.com>
2026-02-19 00:20:57 +0530

    changes in the quiz

--- quiz.py
+++ quiz.py
@@ -12,13 +12,35 @@ question_list = [
     {
         "question": "Define the columns in sql",
         "answer": "A"
+    },
+    {
+        "question":"can we cange the database or tablename or columnname" ,
+        "answer":"B"
+    },
+    {
+        "question":"how we can change the data in the column in sql",
+        "answer":"D"
+    },
+    {
+        "question":"how we can connect a not null and unique column from one table to another table",
+        "answer":"C"
+    },
+    {
+        "question":"what does a truncate command does",
+        "answer":"C"
     }
+    
 ]
 
 option_list = [
     ["A.structured query language" , "B.syntax query language" , "c.summary query lnaguage" , "D.same quesry language"],
     ["A.which consites rows and clom" , "B.collection of data in rows and colums" , "C.which consists rows" , "D.which has data "],
     ["A.which has data in specified object" , "B. which consists of data" , "C.which has data iin cloums" , "D.which has data packed"],
+    ["A.maybe" , "B.YES" , "C.No" , "D.we can but with condition"],
+    ["A.by using delete command" , "B. by using group by command" , "C. by using distinct command" , "D.by using update command"],
+    ["A.Using update command" , "B.By using onlt primary key" , "C. By using primary key and forign key" , "D.we cannot do"],
+    ["A.deletes the complete table" , "B.removes the table from the database" , "C.deletes all the rowns and columns in the table and returns empty table" , "D.deletes the entire database"],
+
 ]
 score = 0
 
