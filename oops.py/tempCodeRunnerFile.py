2c6f99b3faf0bc928bf509f6e40850db45bdebda
nithish kumar <janapalanithish1982715@gmail.com>
2026-02-27 21:51:01 +0530

    new workflow

--- oops.py/tempCodeRunnerFile.py
+++ oops.py/tempCodeRunnerFile.py
@@ -1,14 +1,8 @@
-import time
-history = [ 
-    {"key":"vallu iddaru lovers"} ,
-    {"key":"vallu roju night vesukunrau"},
-    {"key":"roju night yesukovadam valla {p2} detain ayyindi"},
-    {"key":"kani {p2} agadu roju vestune undtadu"}
-]
-p1 = input("Enter the name of boy:")
-p2 = input("enter the name of girl:")
-p3 = input("do you want to calulate flames (yes/no):")
+class otp:
+    def __init__(self , phonenumber):
+        self.phonenumber = phonenumber
 
-for i in range(len(history)):
-    print(history[i]["key"].format(p1=p1 , p2=p2))
-    time.sleep(2)
+    def imp(self):
+        return '*' * 6 + self.phonenumber[-4:]
+o1 = otp("9866736847")
+print(o1.imp())
