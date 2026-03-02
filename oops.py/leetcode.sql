603b2ec72626b2435b43ad0466c45366651a8ec4
nithish kumar <janapalanithish1982715@gmail.com>
2026-03-02 20:25:16 +0530

    sql

--- oops.py/leetcode.sql
+++ oops.py/leetcode.sql
@@ -25,3 +25,11 @@ SELECT
     DENSE_RANK() OVER (ORDER BY score DESC) AS `rank`
 FROM Scores
 ORDER BY score DESC;
+
+
+-- for which employees earn more than their managers
+SELECT e1.name as Employee
+FROM Employee e1
+LEFT JOIN Employee e2
+ON e1.managerId = e2.id
+WHERE e1.salary > e2.salary;
