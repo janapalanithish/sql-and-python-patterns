c1f84e36dd92a5d99031634640451da6a385ecd4
nithish kumar <janapalanithish1982715@gmail.com>
2026-03-03 11:35:19 +0530

    new

--- oops.py/leetcode.py
+++ oops.py/leetcode.py
@@ -13,3 +13,13 @@ if(n1 & 1 == 0):
     print("even")
 else:
     print("odd")
+
+# printing the maximum product of two numbers in a list
+import math
+n = [1 ,2 ,3 , 4, 5]
+max_product = -math.inf
+for i in range(len(n)):
+    for j in range(i+1 , len(n)):
+        print(n[i] , n[j])
+        max_product = max(max_product , n[i]*n[j])
+print(max_product)
--- oops.py/tempCodeRunnerFile.py
+++ oops.py/tempCodeRunnerFile.py
@@ -1,6 +1,8 @@
-# even or odd comparision using bit manipulation
-n1 = int(input("enter the number:"))
-if(n1 & 1 == 0):
-    print("even")
-else:
-    print("odd")
+import math
+n = [1 ,2 ,3 , 4, 5]
+max_product = -math.inf
+for i in range(len(n)):
+    for j in range(i+1 , len(n)):
+        print(n[i] , n[j])
+        max_product = max(max_product , n[i]*n[j])
+print(max_product)
