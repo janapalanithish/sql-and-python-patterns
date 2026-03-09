323b52c342b1de5388d50096f39d186778934895
nithish kumar <janapalanithish1982715@gmail.com>
2026-03-09 18:57:28 +0530

    oops

--- oops.py/oops.py
+++ oops.py/oops.py
@@ -152,4 +152,16 @@ class otp:
 o1 = otp("9866736847")
 print(o1.imp())
 
+class main:
+    def __init__(self , name , discount1 , amount):
+        self.name = name 
+        self.discount1 = discount1
+        self.amount = amount 
+    def offer(self):
+         self.amount = self.amount - (self.amount *self.discount1/100)
+         return self.amount
+a1 = main("nithish" , 10 , 1000)
+print(a1.offer())
+
+
 
--- oops.py/tempCodeRunnerFile.py
+++ oops.py/tempCodeRunnerFile.py
@@ -1,5 +1,10 @@
-s = "nithish"
-h = ""
-if (s == "a" or s == "e" or s == "i" or s == "o" or s == "u"):
-    h = h + s
-print(h)
+class main:
+    def __init__(self , name , discount1 , amount):
+        self.name = name 
+        self.discount1 = discount1
+        self.amount = amount 
+    def offer(self):
+         self.amount = self.amount - (self.amount *self.discount1/100)
+         return self.amount
+a1 = main("nithish" , 10 , 1000)
+print(a1.offer())
