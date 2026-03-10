452b7a821685ac9483593f2ff29e1cdf8fd66809
nithish kumar <janapalanithish1982715@gmail.com>
2026-03-10 19:33:45 +0530

    q2 of this day

--- oops.py/leetcode.sql
+++ oops.py/leetcode.sql
@@ -39,3 +39,17 @@ SELECT num AS ConsecutiveNums
 FROM Logs
 GROUP BY ConsecutiveNums 
 HAVING count(num) > 3;
+
+-- with differnet type of solution 
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 ON l2.id = l1.id + 1
+JOIN Logs l3 ON l3.id = l1.id + 2
+WHERE l1.num = l2.num
+  AND l2.num = l3.num;
+
+-- duplicating email if the email occurs more than once
+SELECT email 
+FROM person 
+GROUP BY email 
+having count(email) > 1
