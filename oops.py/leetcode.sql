9e081c8b3ba26ac5e8b5142204d8f231ab04e089
nithish kumar <janapalanithish1982715@gmail.com>
2026-03-13 17:32:35 +0530

    q3

--- oops.py/leetcode.sql
+++ oops.py/leetcode.sql
@@ -96,3 +96,22 @@ WHERE w2.temperature > w1.temperature;
 SELECT player_id , min(event_date) AS first_login 
 FROM Activity
 GROUP BY player_id;
+
+-- game wise level 4: we need to find the number of palyers are logging in just after the first day after the game and we should return the fraction of total number of users played just after the first day 
+SELECT
+  ROUND(
+    COUNT(A1.player_id)
+    / (SELECT COUNT(DISTINCT A3.player_id) FROM Activity A3)
+  , 2) AS fraction
+FROM
+  Activity A1
+WHERE
+  (A1.player_id, DATE_SUB(A1.event_date, INTERVAL 1 DAY)) IN (
+    SELECT
+      A2.player_id,
+      MIN(A2.event_date)
+    FROM
+      Activity A2
+    GROUP BY
+      A2.player_id
+  );
