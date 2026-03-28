f4c65bc9da84261e705f867467e4857b55a1e340
nithish kumar <janapalanithish1982715@gmail.com>
2026-03-28 09:18:20 +0530

    q2 change

--- oops.py/leetcode.sql
+++ oops.py/leetcode.sql
@@ -318,9 +318,10 @@ END AS type
 FROM Tree;
 
 -- fetching the details of the product which us launched in the first year and quantity 
-select product_id, year as first_year, quantity, price 
-from sales where (product_id, year) in (
-    select product_id, min(year)
-    from sales
-    group by product_id
-)
+SELECT product_id , year AS first_year , quantity , price 
+FROM Sales
+WHERE (product_id , year) IN (
+    SELECT product_id , min(year)
+    FROM Sales
+    GROUP BY product_id
+);
