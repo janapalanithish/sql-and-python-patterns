e23c69470a2d963ca60077ed85542e4fccac2445
nithish kumar <janapalanithish1982715@gmail.com>
2026-03-31 18:35:04 +0530

    q2

--- oops.py/leetcode.sql
+++ oops.py/leetcode.sql
@@ -371,21 +371,19 @@ GROUP BY stock_name;
 
 
 --fetching the details of the people with three or more conscutive id's and people in single id more than 100 or equal. Retrun in the asceding order of id
-WITH filtered AS (
-    SELECT id, visit_date, people,
-           id - ROW_NUMBER() OVER (ORDER BY id) AS grp
-    FROM Stadium
-    WHERE people >= 100
-),
-valid_groups AS (
-    
+WITH filtered AS ( 
+     SELECT id , id - ROW_NUMBER() OVER (ORDER BY id) AS grp
+     FROM stadium 
+     WHERE people >=100
+);
+valid_group AS (
     SELECT grp
     FROM filtered
     GROUP BY grp
     HAVING COUNT(*) >= 3
-)
-
-SELECT id, visit_date, people
-FROM filtered
-WHERE grp IN (SELECT grp FROM valid_groups)
+);
+SELECT id , visit_date , people 
+FROM filtered 
+WHERE grp IN (SELECT grp FROM valid_group)
 ORDER BY visit_date ASC;
+
