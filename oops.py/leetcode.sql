ffb597afff8760b67d4a81121f8437923e03032b
nithish kumar <janapalanithish1982715@gmail.com>
2026-04-01 13:53:38 +0530

    q1

--- oops.py/leetcode.sql
+++ oops.py/leetcode.sql
@@ -387,3 +387,15 @@ FROM filtered
 WHERE grp IN (SELECT grp FROM valid_group)
 ORDER BY visit_date ASC;
 
+--fetching the total amount of trasacations over the period of time and over a location 
+SELECT 
+    DATE_FORMAT(trans_date, '%Y-%m') AS month ,
+    country ,
+    count(id) AS trans_count ,
+    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count ,
+    SUM(amount) AS trans_total_amount ,
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS  approved_total_amount
+FROM Transactions
+GROUP BY month , country;
+
+
