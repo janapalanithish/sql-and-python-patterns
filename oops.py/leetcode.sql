e93d730f625eb3d6c6230728861c171d6fd853bf
nithish kumar <janapalanithish1982715@gmail.com>
2026-04-03 10:08:52 +0530

    q2

--- oops.py/leetcode.sql
+++ oops.py/leetcode.sql
@@ -431,3 +431,14 @@ UNION
 SELECT 'High Salary' AS category
       COUNT(IF(salary > 50000 ,1 , NULL)) AS accounts_count
       FROM Accounts
+
+
+-- fetching the details of product which is ordered more than 100 units in he particular time period 
+
+SELECT product_name , SUM(unit) AS unit 
+FROM Products p1
+LEFT JOIN Orders p2
+ON p1.product_id = p2.product_id
+WHERE order_date between "2020-02-01" and "2020-02-29"
+GROUP BY p1.product_id
+HAVING unit >= 100;
