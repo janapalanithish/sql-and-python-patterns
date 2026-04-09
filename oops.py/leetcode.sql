f42039a6472cf0910cd624084c6435508170ce42
nithish kumar <janapalanithish1982715@gmail.com>
2026-04-09 08:09:23 +0530

    q1

--- oops.py/leetcode.sql
+++ oops.py/leetcode.sql
@@ -507,3 +507,25 @@ group by sell_date
 SELECT tweet_id
 FROM Tweets 
 WHERE LENGTH(content) > 15;
+
+-- fetching the product from three differnet store from a mixing table and reform the table 
+
+SELECT product_id ,
+'store1' AS store ,
+store1 AS price 
+FROM Products 
+WHERE store1 IS NOT NULL
+UNION 
+SELECT product_id , 
+'store2' AS store ,
+store2 AS price 
+FROM Products 
+WHERE store2 IS NOT NULL
+UNION
+SELECT product_id ,
+'store3' AS store ,
+store3 AS price 
+FROM Products
+WHERE store3 IS NOT NULL;
+
+
